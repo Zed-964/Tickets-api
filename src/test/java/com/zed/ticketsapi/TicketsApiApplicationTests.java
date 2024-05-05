9217ca7827037a00bd70a62a70bbecbefde052fc
Zed-964 <enzotechnic@gmail.com>
2024-05-05 21:35:03 +0200

    feat(init) : initial commit

---
+++ src/test/java/com/zed/ticketsapi/TicketsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zed.ticketsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
